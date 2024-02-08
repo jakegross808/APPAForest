0c48c65dcff118883de5164099532841828e8735
jakegross808 <jacob_gross@nps.gov>
2024-02-07 14:56:38 -1000

    edits to acknowledge all available plots and all available eco subsections.

--- x-dist_graphs.R
+++ x-dist_graphs.R
@@ -1,13 +1,87 @@
+# Setup ----
+## Libraries ----
+
+# using Hunter's dev version with personal edits (including accepting a pull request) 
+devtools::load_all('C:/Users/JJGross/Documents/R_projects/rFIA-master/rFIA-master/R')
 library(tidyverse)
 library(modelr)
 library(ggplot2)
 library(units)
 library(gghighlight)
+library(sf)
+library(leaflet)
 
-devtools::load_all('C:/Users/JJGross/Documents/R_projects/rFIA-master/rFIA-master/R')
+library(parallel) # For parallel processing when download/reading FIA data
+cores <- parallel::detectCores(logical = FALSE)-2 # set to use all physical cores minus one.
 
-# all plots ----
-# Using tpa to hopefully get all plots available
+## Download Data ----
+at_states <- c('CT', 'GA', 'ME', 'MD', 'MA', 'NH', 'NJ', 'NY', 'NC', 'PA', 
+               'TN', 'VT', 'VA')
+
+## Small subset of FIA dataset to test 'getFIA()' before big download
+test <- c('AS', 'GU') # smallest datasets in 2023
+
+#FIA_data_here <- './FIA/'
+FIA_data_here <- 'C:/Users/JJGross/Documents/R_projects/FIA_data/allStates'
+
+## change between 'states = test' (to test) and ' states = at_states' (for APPA download) 
+## 'load = FALSE' downloads the data but does not load data into R session 
+## (loading into R session shown in next step with 'readFIA()')
+
+#getFIA(states = test, dir = FIA_data_here, 
+#       nCores = cores, common = TRUE, load = FALSE) 
+
+## Load Data ----
+at <- readFIA(dir = FIA_data_here, states = at_states, nCores = cores)
+
+eco <- sf::read_sf(dsn = './ecoregions', layer = 'at_ecoSub')
+plot(eco["SUBSECTION"])
+plot(eco["SECTION_NA"])
+
+#* eco *missing a ECOSUBCD!!* check ECOSUBCD M211Aa 
+#* will add here and note as 'missing_from_eco'
+missing <- c(PROVINCE = "M211", 
+             SECTION = "M211A", 
+             SUBSECTION = "M211Aa", 
+             SECTION_NA = "Int. Boundary Plat.",
+             SUBSECTI_1 = "Int. Boundary Plat. (eco_NA)")
+
+
+# Clip Data
+at_FIA <- clipFIA(at, mask = eco, matchEval= FALSE, mostRecent = FALSE, nCores = cores)
+at_FIAMR <- clipFIA(at, mask = eco, matchEval= FALSE, mostRecent = TRUE, nCores = cores)
+
+
+## WEIRD!----
+#* *SOMETHING WEIRD HAPPENING HERE* 
+# Why are there 110 plots in most recent 'at_FIAMR' that are not found in entire dataset 'at_FIA'
+
+at_plots <- at_FIA$PLOT %>%
+  mutate(plotcn = format(PLT_CN, scientific = FALSE))
+
+at_plots_MR <- at_FIAMR$PLOT %>%
+  mutate(plotcn = format(PLT_CN, scientific = FALSE))
+
+MRplots_not_in_regular_dataset <- anti_join(at_plots_MR, at_plots, by = join_by(plotcn))
+# ^-----
+
+at_FIAMatch <- clipFIA(at, mask = eco, matchEval= TRUE, mostRecent = FALSE, nCores = cores)
+
+## Check 'matchEval = TRUE' dataset----
+#* *matchEval* 
+# appears to be the same as regular dataset
+
+at_plots <- at_FIA$PLOT %>%
+  mutate(plotcn = format(PLT_CN, scientific = FALSE))
+
+at_plots_Match <- at_FIAMatch$PLOT %>%
+  mutate(plotcn = format(PLT_CN, scientific = FALSE))
+
+check_Matchplots_not_in_regular_dataset <- anti_join(at_plots_Match, at_plots, by = join_by(plotcn, MEASYEAR))
+# ^-----
+
+# Get dataset of all plots to help with location calculations and Ecoregion names
+# Assuming tpa contains all plots since some plots have TPA = 0.
 
 all_plots_lat_long <- tpa(at_FIA, byPlot= TRUE, grpBy = c(STATECD, ECOSUBCD, LAT, LON), method = "annual")
 
@@ -33,7 +107,7 @@ n_distinct(eco$PROVINCE_N)
 levels(as.factor(eco$PROVINCE_N))
 #_______________________________________________________
 
-# ECOSUB and STATE names -------------------------------------------------------
+## ECOSUB and STATE names -------------------------------------------------------
 
 state_codes <- read_csv("us-state-ansi-fips.csv") %>%
   mutate(st = as.integer(st))
@@ -46,11 +120,23 @@ state_eco <- all_plots_lat_long %>%
 # Thinking something like State code plus 4 letter subsection abbr
 
 eco_sub_names <- eco %>%
+  bind_rows(missing) %>%
   select(SECTION, SUBSECTION, SUBSECTI_1) %>%
-  st_drop_geometry()
+  st_drop_geometry() %>%
+  # Make SUBSECTI_1 distinct here:
+  mutate(SUBSECTION_NAME = case_when(SUBSECTI_1 == "Northern Piedmont" & SUBSECTION == "M211Ba" ~ "New Engl. Northern Piedmont",
+                                     SUBSECTI_1 == "Northern Piedmont" & SUBSECTION == "221De" ~ "Appal. Northern Piedmont",
+                                     .default = as.character(SUBSECTI_1))) %>%
+  select(-SUBSECTI_1)
 
 state_eco2 <- state_eco %>%
-  left_join(eco_sub_names, by = join_by(ECOSUBCD == SUBSECTION)) 
+  full_join(eco_sub_names, by = join_by(ECOSUBCD == SUBSECTION)) %>%
+  # Subsection M221Aa has plot but no name in 'eco'
+  # Keep and name this subsection?
+  mutate(SUBSECTION_NAME = case_when(ECOSUBCD == "M211Aa" ~ "Int. Boundary Plat. (eco_NA)",
+                                   .default = as.character(SUBSECTION_NAME))) %>%
+  mutate(SECTION = case_when(ECOSUBCD == "M211Aa" ~ "M211A",
+                                   .default = as.character(SECTION)))
 
 replace_list <- (c("Mountain" = "Mt", 
                   "Central" = "C.", 
@@ -87,7 +173,7 @@ replace_list <- (c("Mountain" = "Mt",
                   ))
 
 state_eco2_custom <- state_eco2 %>%
-  mutate(cust_abbr = str_replace_all(SUBSECTI_1, replace_list))
+  mutate(cust_abbr = str_replace_all(SUBSECTION_NAME, replace_list))
 
 eco_names <- state_eco2_custom %>%
   select(-STATECD, -stname) %>%
@@ -96,10 +182,15 @@ eco_names <- state_eco2_custom %>%
   unite("states", CT:VT, remove = FALSE, na.rm = TRUE, sep = " ") %>%
   mutate(states = paste0("(",states,")")) %>%
   unite("cust_abb_st", cust_abbr:states, remove = FALSE, na.rm = TRUE, sep = " ") %>%
-  select(ECOSUBCD, SECTION, ECOSUB_FULL = SUBSECTI_1, ECOSUB_ST = cust_abb_st, ECOSUB= cust_abbr, STATES = states)
+  select(ECOSUBCD, SECTION, ECOSUB_FULL = SUBSECTION_NAME, ECOSUB_ST = cust_abb_st, ECOSUB= cust_abbr, STATES = states)
+saveRDS(eco_names, "eco_names.rds")
+
+# Check for missing ecosubsections:
+anti_join(eco_sub_names, eco_names, by = join_by(SUBSECTION == ECOSUBCD)) # No FIA Plots result as column 'STATES' = 0
+anti_join(eco_names, eco_sub_names, by = join_by(ECOSUBCD == SUBSECTION)) # Not included in "eco" file
 
 
-# Dist from AT start  ----------------------------------------------------------
+## Dist from AT start  ----------------------------------------------------------
 
 all_plots_spatial <- tpa(at_FIA, byPlot= TRUE, grpBy = c(STATECD, ECOSUBCD, ELEV), method = "annual", returnSpatial = TRUE)
 
@@ -147,7 +238,7 @@ plot_dist <- all_plots_spatial_dist2 %>%
 
 # Add new ECOSUB and dist info to datasets before graphing ------------
 
-# > All Plots ------------
+## BAA All  ------------
 
 # BAA Boxplot graphs by ECOSUBCD
 BAA_plots_spatial <- tpa(at_FIA, 
@@ -230,7 +321,7 @@ ECO_n %>%
 
 
 
-# > Most Recent Plots ------------
+## BAA Most Recent ------------
    
 # BAA Boxplot graphs by ECOSUBCD
 BAA_plots_spatial_MR <- tpa(at_FIAMR, 
@@ -239,20 +330,36 @@ BAA_plots_spatial_MR <- tpa(at_FIAMR,
                          method = "annual", 
                          returnSpatial = TRUE) 
 
+
+## WEIRD!----
+#* *SOMETHING WEIRD HAPPENING HERE* 
+# Why are there 47 plots in most recent 'at_FIAMR' after 'tpa()' that are not found in entire dataset 'at_FIA' after tpa()
+compare_BAA_plots_spatial_MR <- BAA_plots_spatial_MR %>%
+  st_drop_geometry() 
+
+compare_BAA_plots_spatial <- BAA_plots_spatial %>%
+  st_drop_geometry() 
+
+MR_BAA_plots_not_in_regular_dataset <- anti_join(compare_BAA_plots_spatial_MR, compare_BAA_plots_spatial, by = join_by(PLT_CN))
+# ^-----
+
+
 BAA_graph_MR <- BAA_plots_spatial_MR %>%
-  left_join(plot_dist, by = join_by(pltID), relationship = "many-to-one", keep = FALSE) %>%
+  left_join(plot_dist, by = join_by(pltID), relationship = "many-to-one", keep = FALSE) %>% #keep = FALSE means only keys from x are retained
   left_join(eco_names, by = join_by(ECOSUBCD), relationship = "many-to-one")
 
 BAA_graph_MR %>%
   ggplot(aes(x=dist, y=BAA, color = ECOSUB_ST)) +
-  geom_point()  
-
+  geom_point() 
+# Warning message:
+#   Removed 47 rows containing missing values (`geom_point()`). 
+# This is because of problem highlighted above - 47 plots in _MR dataset that are not in full dataset...
 
 first_quantile <- quantile(BAA_graph_MR$BAA, 0.25)
 
 BAA_graph_MR %>%
   st_drop_geometry() %>%
-  mutate(ECOSUB_ST = forcats::fct_reorder(ECOSUB_ST, dist)) %>%
+  mutate(ECOSUB_ST = forcats::fct_reorder(ECOSUB_ST, dist, .na_rm = FALSE)) %>%
   ggplot() +
   geom_boxplot(aes(x=ECOSUB_ST, y=BAA), color = "red") +
   gghighlight(median(BAA) < first_quantile) +
@@ -261,6 +368,120 @@ BAA_graph_MR %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   xlab("Ecosubsection (from SW to NE)") + ylab("Basal area (sq. ft.) per acre") +
   geom_text(aes(1.5,first_quantile,label = "Q1", vjust = -0.5))
+
+# Regen by Plot ----
+
+
+## Seedlings ----
+
+#Seedlings size:
+#<2.54 cm diameter at breast height (DBH) and ≥15.24 or 30.48 cm
+#tall for conifer and deciduous species,
+
+# Stems/sq. meter
+# seedling densities between 0.1 and 0.25 stems/m2
+# (1,000–2,500 stems/ha) and sapling densities of 0.01–0.1 stems/
+#  m2 (100–1,000 stems/ha) are frequently cited as insufficient 
+# (e.g. Abrams & Johnson, 2012; Bressette, Beck, & Beauchamp, 2012; Russell et al., 2017).
+seedling_regen_failure <- 0.25 # per square meter
+threshold <- seedling_regen_failure
+
+# Get dataset:-----------------------------------------------------------------.
+seedlings_plots <- seedling(at_FIA, 
+                         byPlot= TRUE, 
+                         method = "annual", 
+                         returnSpatial = TRUE) 
+#------------------------------------------------------------------------------.
+
+# Add Ecoregion distance from start and Ecoregion names
+seedlings_plots_graph <- seedlings_plots %>%
+  left_join(plot_dist, by = join_by(pltID), relationship = "many-to-one", keep = FALSE) %>%
+  left_join(eco_names, by = join_by(ECOSUBCD), relationship = "many-to-one") %>%
+  mutate(seedlings_per_m = TPA/4046.86)
+
+#* *Some SUBSECTIONs do not have plots!*
+#1 M221B   M221Be     Western Allegheny Mountain and Valley
+#2 221J    221Ja      Rolling Limestone Hills              
+#3 231A    231Ad      Lower Foot Hills
+
+eco_sub_names %>%
+  anti_join(seedlings_plots_graph, by = join_by(SUBSECTION == ECOSUBCD))
+
+# Boxpot Ecoregion graph
+seedlings_plots_graph %>%
+  st_drop_geometry() %>%
+  mutate(ECOSUB_ST = forcats::fct_reorder(ECOSUB_ST, dist)) %>%
+  ggplot() +
+  geom_boxplot(aes(x=ECOSUB_ST, y=seedlings_per_m), color = "red") +
+  gghighlight(median(seedlings_per_m) < threshold) +
+  geom_hline(yintercept = threshold, linetype="dashed", color = "black") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  xlab("Ecosubsection") + ylab("seedlings per acre") +
+  geom_text(aes(1.5,threshold,label = "Q1", vjust = -0.5))
+
+### most recent ----------------------------------------------------------------
+
+# Get Datadset
+seedlings_plots_MR <- seedling(at_FIAMR, 
+                            byPlot= TRUE, 
+                            method = "annual", 
+                            returnSpatial = TRUE) 
+#------------------------------------------------------------------------------.
+
+# Add Ecoregion distance from start and Ecoregion names
+seedlings_MR <- seedlings_plots_MR %>%
+  left_join(plot_dist, by = join_by(pltID), relationship = "many-to-one", keep = FALSE) %>%
+  left_join(eco_names, by = join_by(ECOSUBCD), relationship = "many-to-one") %>%
+  # Change TPA to trees (seedlings) per square meter
+  mutate(seedlings_per_m = TPA/4046.86) %>%
+  mutate(seedlings_below_threshold = case_when(seedlings_per_m < threshold ~ TRUE, 
+                                               .default = FALSE)) 
+
+# Boxpot Ecoregion graph (most recent)
+look_seedlings_MR <- seedlings_MR %>%
+  st_drop_geometry() %>%
+  mutate(ECOSUB_ST = forcats::fct_reorder(ECOSUB_ST, dist)) %>%
+  ggplot() +
+  geom_boxplot(aes(x=ECOSUB_ST, y=seedlings_per_m), color = "red") +
+  gghighlight(median(seedlings_per_m) < threshold) +
+  geom_hline(yintercept = threshold, linetype="dashed", color = "black") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  xlab("Ecosubsection") + ylab("seedlings per acre") +
+  geom_text(aes(1.5,threshold,label = "Q1", vjust = -0.5))
+
+# ggplot
+seedlings_MR %>%
+  ggplot() +
+  geom_sf(aes(geometry = geometry, color = seedlings_below_threshold), shape = 21) +
+  gghighlight(seedlings_below_threshold == TRUE, use_group_by = FALSE) 
+
+# leaflet
+pal <- colorFactor(
+  c("#999999", "#FF4E50"),
+  # colors depend on the count variable
+  domain = seedlings_MR$seedlings_below_threshold,
+)
+
+seedlings_MR %>%  
+  leaflet() %>%
+  addTiles() %>%
+  addCircleMarkers(
+    color = ~pal(seedlings_below_threshold),
+    # set the opacity of the circles
+    opacity = 0.65,
+    # set the radius of the circles
+    radius = 4,) %>%
+  addLegend(
+    #data = pharmacies_count,
+    pal = pal,
+    values = ~seedlings_below_threshold,
+    position = "bottomleft",
+    title = "Seedlings below threshold",
+    opacity = 0.9
+  )
+
+
+
 
 
 # Invasive Cover by Plot ----
